963467030c577e33e833def2b356180297b5ff2d
Andrey Markov <andrewshasucks@gmail.com>
2021-04-12 02:26:12 +1000

    tests update

--- tests.cpp
+++ tests.cpp
@@ -4,7 +4,7 @@
 #include "queue.h"
 
 
-TEST(QueueAdd, FullRingAdding) {
+TEST(QueueAdd, FullQueueRingAdding) {
     // делаем круги в полных очередях, пока не сделаем полный круг
     for (unsigned int size = 1; size <= 10; ++size) {
         auto queue = Queue(size);
@@ -17,19 +17,22 @@ TEST(QueueAdd, FullRingAdding) {
         // проверяем, что действительно добавлено size элементов
         EXPECT_EQ(queue.getLength(), size);
 
-        // делаем 1 полный круг с заполенной полностью очередью
-        auto index = size;
+        // делаем 1 полный круг с заполенной полностью очередью и проверяем каждый элемент очереди
+        unsigned int index = 1;
+        auto value = int(size);
         for (int i = 0; i < size + 2; ++i) {
             queue.pop();
-            queue.add(int(index + 1));
-            EXPECT_EQ(queue.getVector()[index % (size + 1)], int(index + 1));
+            queue.add(++value);
+            for (unsigned int j = index, k = 0; k < size; ++j, ++k) {
+                EXPECT_EQ(queue.getVector()[j % (size + 1)], value - size + k + 1);
+            }
             EXPECT_EQ(queue.getLength(), size);
-            ++index;
+            index = (index + 1) % (size + 1);
         }
     }
 }
 
-TEST(QueueAdd, HalfRingAdding) {
+TEST(QueueAdd, HalfQueueRingAdding) {
     // делаем круги в заполненных наполовину очередях, пока не сделаем полный круг
     // (для 1 не работает, т.к. очередь сначала ничем не заполняется, а затем происходит вызов метода pop)
     for (unsigned int size = 2; size <= 10; ++size) {
@@ -44,14 +47,17 @@ TEST(QueueAdd, HalfRingAdding) {
         // проверяем, что действительно добавлено halfSize элементов
         EXPECT_EQ(queue.getLength(), halfSize);
 
-        // делаем 1 полный круг с заполенной наполовину очередью
-        auto index = halfSize;
+        // делаем 1 полный круг с заполенной наполовину очередью и проверяем каждый элемент очереди
+        unsigned int index = 1;
+        auto value = int(halfSize);
         for (int i = 0; i < size + 2; ++i) {
             queue.pop();
-            queue.add(int(index + 1));
-            EXPECT_EQ(queue.getVector()[index % (size + 1)], int(index + 1));
+            queue.add(++value);
+            for (unsigned int j = index, k = 0; k < halfSize; ++j, ++k) {
+                EXPECT_EQ(queue.getVector()[j % (size + 1)], value - halfSize + k + 1);
+            }
             EXPECT_EQ(queue.getLength(), halfSize);
-            ++index;
+            index = (index + 1) % (size + 1);
         }
     }
 }
